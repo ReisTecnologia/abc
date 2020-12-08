84c3effc73444bb2f7531cbdd97581f28a1a2f48
Bruno Reis <bruno.p.reis@gmail.com>
2020-12-08 12:33:41 -0300

    first prototype

--- src/App.js
+++ src/App.js
@@ -1,50 +1,31 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import React from "react"
+import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { gql, useQuery } from '@apollo/client'
+import { ApolloProvider } from '@apollo/client'
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
+const client = new ApolloClient({
+  uri: "/.netlify/functions/graphql",
+  cache: new InMemoryCache(),
+})
+
+
+const HELLO = gql`
+  query Hello {
+    hello
   }
+`
 
-  handleClick = api => e => {
-    e.preventDefault()
 
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
+const ApolloApp = (Wrapped) => (
+  <ApolloProvider client={client}>
+    <Wrapped />
+  </ApolloProvider>
+)
 
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
+const Wrapped = () => {
+  const { loading, error, data } = useQuery(HELLO)
+  console.log("data", {data, error, loading})
+  return <span> dude </span>
 }
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
-      </div>
-    )
-  }
-}
-
-export default App
+export default () => ApolloApp(Wrapped)
