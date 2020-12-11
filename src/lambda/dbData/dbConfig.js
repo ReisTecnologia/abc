d8efe1f86ab58e18c5bb5c653050bf92907553ea
Caio <caiopcmaia94@gmail.com>
2020-12-11 10:56:48 -0300

    setting up enviroment variables

--- src/lambda/dbData/dbConfig.js
+++ src/lambda/dbData/dbConfig.js
@@ -4,8 +4,8 @@ module.exports = {
     //Provide details for local configuration
   },
   aws_remote_config: {
-    accessKeyId: 'AKIA2VL5KESILJPGH4HI',
-    secretAccessKey: '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',
+    accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY,
     region: 'sa-east-1',
   },
 }
