3500d8304450349e8070c222665167fe15b9b9d5
Caio <caiopcmaia94@gmail.com>
2020-12-11 11:14:42 -0300

    updated master

--- src/EditLesson.js
+++ src/EditLesson.js
@@ -3,7 +3,6 @@ import { gql, useQuery } from '@apollo/client'
 import { EditableLesson } from './EditableLesson'
 import { useParams, useRouteMatch, Switch, Route } from 'react-router-dom'
 
-
 export const EditLesson = () => {
   let { path } = useRouteMatch()
   return (
