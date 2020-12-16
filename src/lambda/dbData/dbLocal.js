de9939e66fd96fc725dc899a46f66eeb3e34b46a
Caio <caiopcmaia94@gmail.com>
2020-12-16 15:52:17 -0300

    createTable inside try catch flow

--- src/lambda/dbData/dbLocal.js
+++ src/lambda/dbData/dbLocal.js
@@ -6,49 +6,41 @@ AWS.config.update(config.aws_local_config)
 
 var dynamodb = new AWS.DynamoDB()
 
-var params = {}
-
-const tableList = dynamodb.listTables(params, function (err, data) {
-  if (err) console.log(err, err.stack)
-  else console.log(data)
-})
-
-const checkTable = () => {
-  const createTableParams = {
-    AttributeDefinitions: [
-      {
-        AttributeName: 'id',
-        AttributeType: 'S',
-      },
-      {
-        AttributeName: 'name',
-        AttributeType: 'S',
-      },
-    ],
-    KeySchema: [
-      {
-        AttributeName: 'id',
-        KeyType: 'HASH',
-      },
-      {
-        AttributeName: 'name',
-        KeyType: 'RANGE',
-      },
-    ],
-    ProvisionedThroughput: {
-      ReadCapacityUnits: 5,
-      WriteCapacityUnits: 5,
+const createTableParams = {
+  AttributeDefinitions: [
+    {
+      AttributeName: 'id',
+      AttributeType: 'S',
     },
-    TableName: 'lessons',
-  }
-  if (tableList.length === 0)
-    return dynamodb.createTable(createTableParams, function (err, data) {
-      if (err) console.log(err, err.stack)
-      else console.log(data)
-    })
-  else console.log('table already exists')
+    {
+      AttributeName: 'name',
+      AttributeType: 'S',
+    },
+  ],
+  KeySchema: [
+    {
+      AttributeName: 'id',
+      KeyType: 'HASH',
+    },
+    {
+      AttributeName: 'name',
+      KeyType: 'RANGE',
+    },
+  ],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 5,
+    WriteCapacityUnits: 5,
+  },
+  TableName: 'lessons',
 }
 
-module.exports = {
-  checkTable,
+try {
+  dynamodb.createTable(createTableParams, function (err, data) {
+    if (err) console.log(err, err.stack)
+    else console.log(data)
+  })
+} catch (err) {
+  throw 'error'
+} finally {
+  console.log('In case of ResourceInUseException, table already exists')
 }
