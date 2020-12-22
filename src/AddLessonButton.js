38f00363c72e782e8f98295deaa739d7b562a799
Caio <caiopcmaia94@gmail.com>
2020-12-22 15:23:38 -0300

    console.log(randomID)

--- src/AddLessonButton.js
+++ src/AddLessonButton.js
@@ -36,7 +36,7 @@ export const AddLessonButton = () => {
 
   return (
     <div>
-      <button onClick={console.log(randomID)}>+</button>
+      <button onClick={(addLesson, console.log(randomID))}>+</button>
     </div>
   )
 }
