28ea3ee2ae936614b4d1fa3b201455eee031369a
Caio <caiopcmaia94@gmail.com>
2020-12-23 17:02:46 -0300

    removed test

--- src/AddLessonButton.js
+++ src/AddLessonButton.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { gql, useMutation } from '@apollo/client'
 
 export const ADD_LESSON = gql`
-  mutation addLesson($id: String! = "test") {
+  mutation addLesson($id: String!) {
     addLesson(id: $id) {
       id
       name
