02909e8c3d304b93bb591934c1b527d9e6a23db5
Caio <caiopcmaia94@gmail.com>
2020-12-23 14:57:18 -0300

    testing default id

--- src/AddLessonButton.js
+++ src/AddLessonButton.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { gql, useMutation } from '@apollo/client'
 
 export const ADD_LESSON = gql`
-  mutation addLesson($id: String!) {
+  mutation addLesson($id: ID = test) {
     addLesson(id: $id) {
       id
       name
