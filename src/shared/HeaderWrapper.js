084c0dd3eed4578060c0e3ef3bd9a1d4d06d7037
Caio <caiopcmaia94@gmail.com>
2021-03-10 18:26:11 -0300

    header fix

--- src/shared/HeaderWrapper.js
+++ src/shared/HeaderWrapper.js
@@ -1,28 +1,13 @@
 import styled from 'styled-components'
 
 export const HeaderWrapper = styled.div`
-  @media (min-width: 540px) {
-    display: flex;
-    padding: 5px 20px;
-    width: 100%;
-    background: var(--primary-color);
-    color: #fff;
-    position: fixed;
-    top: 0;
-    height: 2rem;
-    z-index: 2;
-  }
-
-  @media (max-width: 539px) {
-    display: flex;
-    flex-wrap: wrap;
-    padding: 5px 20px;
-    width: 100vw;
-    background: var(--primary-color);
-    color: #fff;
-    position: fixed;
-    top: 0;
-    height: 3.7rem;
-    z-index: 2;
-  }
+  display: flex;
+  padding: 5px 20px;
+  width: 100%;
+  background: var(--primary-color);
+  color: #fff;
+  position: fixed;
+  top: 0;
+  height: 2rem;
+  z-index: 2;
 `
