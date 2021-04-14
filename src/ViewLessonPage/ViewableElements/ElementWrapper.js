45887ad8a0db65434b67103fef43ec6663b9f8cf
Caio <caiopcmaia94@gmail.com>
2021-04-14 18:02:06 -0300

    margin in large screens

--- src/ViewLessonPage/ViewableElements/ElementWrapper.js
+++ src/ViewLessonPage/ViewableElements/ElementWrapper.js
@@ -2,6 +2,10 @@ import styled from 'styled-components'
 
 export const ElementWrapper = styled.div`
   margin-top: ${({ first }) => (first ? '15px' : '45px')};
-  margin-left: 25px;
-  margin-right: 25px;
+  margin-left: 25%;
+  margin-right: 25%;
+  @media (max-width: 720px) {
+    margin-left: 25px;
+    margin-right: 25px;
+  }
 `
