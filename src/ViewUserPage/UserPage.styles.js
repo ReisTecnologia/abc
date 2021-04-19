978987999b0dfc319bfe8f7012cbe20bf4934ff7
Caio <caiopcmaia94@gmail.com>
2021-04-19 15:51:10 -0300

    menu and user page styles

---
+++ src/ViewUserPage/UserPage.styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-bottom: solid 1px #ccc;
+`
+export const InfoWrapper = styled.div`
+  padding-top: 10px;
+  padding-bottom: 10px;
+`
+export const Label = styled.label`
+  padding-right: 5px;
+`
+export const UserWrapper = styled.div``
